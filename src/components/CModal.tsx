fa693bd8f9a9080f51cad4362cb3c0032dd466b5
Ali Balouchi <alibalouchi.74@gmail.com>
2022-05-04 08:12:34 -0400

    - Created modal functionality - Removed unwanted lines of code

---
+++ src/components/CModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    children: JSX.Element | undefined
+}
+
+const CModal = ({children}:Props) => {
+  return (
+    <div>CModal</div>
+  )
+}
+
+export default CModal
---
+++ src/contexts/ModalContext.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, ReactNode, useState } from "react";
+import Modal from '../components/CModal'
+
+interface IModal {
+    open: () => void
+    close: () => void
+    setModalContent: (modalContent: JSX.Element) => void
+    visible: boolean
+}
+
+type Props = {
+    children: ReactNode
+}
+
+export const ModalContext = createContext<IModal>({open: () => {}, close: () => {}, setModalContent: () => {}, visible: false})
+
+export const ModalProvider = ({ children }: Props) => {
+    const [showModal, setShowModal] = useState<boolean>(false)
+    const [modalContent, _setModalContent] = useState<JSX.Element>()
+
+    const open = () => setShowModal(true)
+    const close = () => setShowModal(false)
+    const setModalContent = (modalContent: JSX.Element) => {
+        _setModalContent(modalContent)
+    }
+
+    return <ModalContext.Provider value={{ open, close, setModalContent, visible: showModal }}>
+        {children}
+        {showModal ?
+            <Modal>
+                {modalContent}
+            </Modal>
+        : null}
+    </ModalContext.Provider>
+}
