08b2b8bf91d092b77328a62d91dbe3afe165fb42
Ali Balouchi <alibalouchi.74@gmail.com>
2022-08-25 01:06:04 -0400

    add disable outside click feature

--- src/components/CModal.tsx
+++ src/components/CModal.tsx
@@ -4,11 +4,12 @@ import OutsideClick from './OutsideClick'
 type Props = {
     children: JSX.Element | undefined;
     styles?: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
-    onDismiss: () => void
+    onDismiss: () => void,
+    disableOutsideClick?: boolean
 }
 
-const CModal = ({ children, styles, onDismiss }: Props) => {
-    return <OutsideClick callBack={onDismiss}>
+const CModal = ({ children, styles, onDismiss, disableOutsideClick }: Props) => {
+    return <OutsideClick callBack={disableOutsideClick ? () => {} : () => onDismiss()}>
         <div style={{
             ...styles, padding: "5px",
             backgroundColor: "white"
--- src/contexts/ModalContext.tsx
+++ src/contexts/ModalContext.tsx
@@ -9,12 +9,15 @@ interface IModal {
 }
 
 type Props = {
-    children: ReactNode
+    children: ReactNode,
+    options?: {
+        disableDismissOnOutsideClick: boolean
+    }
 }
 
 export const ModalContext = createContext<IModal>({open: () => {}, close: () => {}, setModalContent: () => {}, visible: false})
 
-export const ModalProvider = ({ children }: Props) => {
+export const ModalProvider = ({ children, options }: Props) => {
     const [showModal, setShowModal] = useState<boolean>(false)
     const [modalContent, _setModalContent] = useState<JSX.Element>()
 
@@ -27,7 +30,7 @@ export const ModalProvider = ({ children }: Props) => {
     return <ModalContext.Provider value={{ open, close, setModalContent, visible: showModal }}>
         {children}
         {showModal ?
-            <Modal onDismiss={close}>
+            <Modal onDismiss={close} disableOutsideClick={options?.disableDismissOnOutsideClick}>
                 {modalContent}
             </Modal>
         : null}
